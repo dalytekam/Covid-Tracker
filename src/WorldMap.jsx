6b3429ac01ae99c90bbe300adf1a1d6198905225
daly richy <dalyeu@yahoo.fr>
2020-10-30 06:30:21 -0400

    worked on responsiveness

--- src/WorldMap.jsx
+++ src/WorldMap.jsx
@@ -6,7 +6,7 @@ import drawCirclesOnTheMap from "./dynamicData"
 
 const WorldMap = ({mapCenter, mapZoom, countries,type }) => {
   return (
-    
+   <>
        <Map center={mapCenter} zoom={mapZoom}>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -14,7 +14,9 @@ const WorldMap = ({mapCenter, mapZoom, countries,type }) => {
         />
        {drawCirclesOnTheMap(countries,type)}
       </Map>
-   
+      <p>source: disease.sh.</p>
+      <p>Built with love by <a href="https://dalytekam.github.io/" target ="_blank">Daly tekam</a></p>
+    </>
   );
 };
 
